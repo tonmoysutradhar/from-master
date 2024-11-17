3c9c1ce322d8474127232e13997904a5cb356d9d
tonmoysutradhar <tonmoykumar933@gmail.com>
2024-11-18 04:41:29 +0600

    Handle form with onSubmit and access field data

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import SimpleFrom from './components/SimpleFrom/SimpleFrom'
+
+function App() {
+
+  return (
+    <>
+
+      <h2>From Master</h2>
+      <SimpleFrom></SimpleFrom>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/SimpleFrom/SimpleFrom.jsx
@@ -0,0 +1,28 @@
+
+
+const SimpleFrom = () => {
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        console.log(e.target.name.value);
+        console.log(e.target.email.value);
+        console.log(e.target.phone.value);
+        
+        console.log('From submitted');
+        
+    }
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" />
+                <br />
+                <input type="email" name="email" />
+                <br />
+                <input type="number" name="phone" />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default SimpleFrom;
